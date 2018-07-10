dbd964997d59444ff186630bd4df2545303445a8
AnnabelleCat <pikainka@gmail.com>
2018-07-10 18:12:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizz_buzz
   number=gets.chomp
-  if number%3 == 0 && number%5 == 0
+if number%3 == 0 && number%5 == 0
   puts "fizz buzz"
+elsif number%3 == 0
+  puts "fizz"
+else number%5 == 0
+  puts "buzz"
 end
 
 fizz_buzz
