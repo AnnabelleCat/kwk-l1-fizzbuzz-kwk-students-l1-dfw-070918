e8bf9554f2c7899acac131a6d008bd8d3ee24488
AnnabelleCat <pikainka@gmail.com>
2018-07-10 18:28:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizz_buzz
 number= 4
-puts number
 if number%3==0 && number%5==0
   puts "FizzBuzz"
 elsif number%3==0
   puts "Fizz"
 elsif number%5==0
   puts "Buzz"
+else
+  puts "nil"
 end
 end
 
